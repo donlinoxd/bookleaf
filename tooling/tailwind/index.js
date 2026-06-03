911354b3ced7fa898f7958cf8cddd0da31574267
donlinoxd <don.lino.xd@gmail.com>
2026-06-03 17:34:48 +0800

    fix: export theme.extend content from tailwind-config (not theme.extend wrapper)

--- tooling/tailwind/index.js
+++ tooling/tailwind/index.js
@@ -1,18 +1,16 @@
-/** @type {import('tailwindcss').Config['theme']} */
+/** @type {import('tailwindcss').Config['theme']['extend']} */
 module.exports = {
-  extend: {
-    colors: {
-      brand: {
-        DEFAULT: '#2A5C33',
-        light: '#3A7A45',
-        dark: '#1C3E23',
-      },
-      mint: {
-        DEFAULT: '#E2EFE0',
-        dark: '#C8DFC5',
-      },
-      leaf: '#5CB85C',
-      bio: '#FAFDF9',
+  colors: {
+    brand: {
+      DEFAULT: '#2A5C33',
+      light: '#3A7A45',
+      dark: '#1C3E23',
     },
+    mint: {
+      DEFAULT: '#E2EFE0',
+      dark: '#C8DFC5',
+    },
+    leaf: '#5CB85C',
+    bio: '#FAFDF9',
   },
 };
